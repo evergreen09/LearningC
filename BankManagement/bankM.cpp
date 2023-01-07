7afe7698422d4c5fceceaa517cd5d03d943b9a16
evergreen09 <j364254@gmail.com>
2023-01-07 23:18:03 +0900

    "Started a BankManagement project. Finished the Frontend part  of project by creating CLI."

---
+++ BankManagement/bankM.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+void new_acc() {
+    cout << "Creates a new customer account\n";
+}
+
+void view_list() {
+    cout << "View customer's banking info\n";
+}
+
+void edit() {
+    cout << "Change account info of a particular account\n";
+}
+
+void transact() {
+    cout << "Change address or phone number of a particular account\n";
+}
+
+void erase() {
+    cout << "Deletes a customer account\n";
+}
+
+void acc_detail() {
+    cout << "Shows an account information\n";
+}
+
+void menu() {
+
+    int option;
+
+    cout << "Welcome to BullShit Bank~!\n" << "Please select one of the menu below to proceed.\n";
+    cout << "1) Create an account.\n";
+    cout << "2) Account Info\n";
+    cout << "3) Account Info Update\n";
+    cout << "4) Wire Transfer\n";
+    cout << "5) Account Deletion\n";
+    cout << "6) Bank Account inquiry\n";
+    cout << "7) EXIT\n";
+
+    cin >> option;
+
+for(int i = 0; i < 2; i++) {
+    if (option > 0 && option < 8) {
+        switch(option) {
+        case 1:
+            new_acc();
+            break;
+        case 2:
+            view_list();
+            break;
+        case 3:
+            edit();
+            break;
+        case 4:
+            transact();
+            break;
+        case 5:
+            erase();
+            break;
+        case 6:
+            acc_detail();
+            break;
+        case 7:
+            abort();
+        }
+    } else {
+        cout << "Please Enter a Valid Option or Program will terminate!\n";
+        cin >> option;
+    }
+}
+    
+
+}
+
+
+
+int main() {
+
+    menu();
+
+    return 0;
+}
