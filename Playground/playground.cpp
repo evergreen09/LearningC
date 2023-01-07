3119b8ff880b746f37b4365ac3e4a3aa100db1b3
evergreen09 <j364254@gmail.com>
2023-01-07 18:55:26 +0900

    "practiced OOP in playground.cpp"

--- Playground/playground.cpp
+++ Playground/playground.cpp
@@ -3,7 +3,30 @@
 
 using namespace std;
 
+class Player {
+    public:
+        int level;
+        string name;
+        string type;
+        Player (int aLevel, string aName, string aType) {
+            level = aLevel;
+            name = aName;
+            type = aType;
+        }
+
+        void dungeonL() {
+            if (level > 10) {
+                cout << name << " can enter this dungeon!\n";
+            }
+            cout << name << " cannot enter this dungeon!\n" << "Player must level up " << 15 - level << " times!\n";
+        }
+};
+
 int main() {
-    cout << "Test";
+    
+    Player player1(9, "Gandalf", "Wizard");  
+
+    player1.dungeonL();
+
     return 0;
 }
